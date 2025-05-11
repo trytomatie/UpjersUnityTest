2c157f10cdd0056a37824360d23e65c79214bb68
Trytomatie <trytomatie@gmail.com>
2025-05-11 02:57:04 +0200

    Growth Cycle Adjustments and ProgressBar UI Functionality Added

---
+++ Upjers Unity Test/Assets/ProgressBar.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlantingGame
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        public Image increaseBar;
+        public Image fillBar;
+        public GameObject progressBarParent;
+        public TextMeshProUGUI progressText;
+        public GameObject workerAlertParent;
+        public TextMeshProUGUI workerAlertText;
+        public float fillDuration = 0.5f;
+        public float backFillDelay = 0.1f;
+        public float backFillSpeed = 0.1f;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            HideWorkerAlert();
+        }
+
+        /// <summary>
+        /// Update progress from 0 to 1
+        /// </summary>
+        /// <param name="progress"></param>
+        public void UpdateProgressBar(float progress)
+        {
+            StartCoroutine(UpdateProgressBarCoroutine(progress));
+        }
+
+        /// <summary>
+        /// Show a worker alert with a message.
+        /// </summary>
+        /// <param name="text"></param>
+        public void ShowWorkerAlert(string text)
+        {
+            progressBarParent.SetActive(false);
+            workerAlertParent.SetActive(true);
+            workerAlertText.text = text;
+        }
+
+        /// <summary>
+        /// Show a worker alert with a message.
+        /// </summary>
+        /// <param name="text"></param>
+        public void ShowWorkerAlert(string text,Color color)
+        {
+            workerAlertText.color = color;
+            ShowWorkerAlert(text);
+        }
+
+        public void HideWorkerAlert()
+        {
+            progressBarParent.SetActive(true);
+            workerAlertParent.SetActive(false);
+        }
+
+        IEnumerator UpdateProgressBarCoroutine(float targetProgress)
+        {
+            float startProgress = fillBar.fillAmount;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < fillDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                float progress = Mathf.Lerp(startProgress, targetProgress, elapsedTime / fillDuration);
+                increaseBar.fillAmount = progress;
+                print(progress);
+                yield return null;
+            }
+            yield return new WaitForSeconds(backFillDelay);
+            elapsedTime = 0;
+            while (elapsedTime < backFillSpeed)
+            {
+                elapsedTime += Time.deltaTime;
+                float progress = Mathf.Lerp(startProgress, targetProgress, elapsedTime / backFillSpeed);
+                fillBar.fillAmount = progress;
+                yield return null;
+            }
+        }
+    }
+}
+
--- Upjers Unity Test/Assets/Scripts/Crop.cs
+++ Upjers Unity Test/Assets/Scripts/Crop.cs
@@ -8,51 +8,68 @@ namespace PlantingGame
         public float growthTime = 15f;
         public float cost = 10f;
         public float sellPriceMultiplier = 1.5f;
+        public bool canGrow = true;
         public GrowthStage currentGrowthStage = GrowthStage.New;
+        public GameObject progressBarPrefab;
         public UnityEvent onGrowthCycleUpdate;
-
+        private bool _waitingForWorker = false;
         private float _advancmentCompletionTime;
         private PlantingManager _plantingManager;
+        private ProgressBar _progressBar;
+
 
         private void Start()
         {
             CropSetup();
+            Canvas canvas = FindFirstObjectByType<Canvas>();
             _plantingManager = FindFirstObjectByType<PlantingManager>();
             if(_plantingManager == null)
             {
                 Debug.LogError("PlantingManager not found in the scene.");
                 return;
             }
+            _progressBar = Instantiate(progressBarPrefab, canvas.transform).GetComponent<ProgressBar>();
+            _progressBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 1.5f, 0));
+            onGrowthCycleUpdate.AddListener(UpdateProgressBar);
+
+        }
+
+        public void UpdateProgressBar()
+        {
+            float progress = 1-(_advancmentCompletionTime - Time.time) / growthTime;
+            _progressBar.UpdateProgressBar(progress);
+            _progressBar.progressText.text = $"{(int)(progress * 100)}%";
         }
         /// <summary>
         /// Logic for planting a crop.
         /// </summary>
         private void CropSetup()
         {
-            _advancmentCompletionTime = Time.time;
+            _advancmentCompletionTime = Time.time + growthTime;
             currentGrowthStage = GrowthStage.New;
-            StartCoroutine(AdvanceGrowthCycle());
+            StartCoroutine(CheckGrowthCycle());
         }
 
-        IEnumerator AdvanceGrowthCycle()
+        IEnumerator CheckGrowthCycle()
         {
-            while (true)
+            while (canGrow)
             {
                 switch(currentGrowthStage)
                 {
                     case GrowthStage.New:
                         if (CheckGrowthAdvancement())
                         {
-                            currentGrowthStage = GrowthStage.Maturing;
-                            _advancmentCompletionTime = float.MaxValue; // Reset the advancement completion time
                             // TODO: Call worker here
+                            _waitingForWorker = true;
+                            _progressBar.ShowWorkerAlert("Water!",new Color32(154,255,255,255));
                         }
                         break;
                     case GrowthStage.Maturing:
                         if (CheckGrowthAdvancement())
                         {
-                            currentGrowthStage = GrowthStage.Harvestable;
+                            CheckGrowthCycle();
                             // TODO: Call worker here
+                            _progressBar.ShowWorkerAlert("Harvest!",Color.yellow);
                         }
                         break;
                     case GrowthStage.Harvestable:
@@ -64,12 +81,28 @@ namespace PlantingGame
             }
         }
 
+        public void AdvanceGrowthCycle()
+        {
+            if (currentGrowthStage == GrowthStage.New)
+            {
+                _waitingForWorker = false;
+                _progressBar.HideWorkerAlert();
+                currentGrowthStage = GrowthStage.Maturing;
+                _advancmentCompletionTime = Time.time + growthTime;
+            }
+            else if (currentGrowthStage == GrowthStage.Maturing)
+            {
+                currentGrowthStage = GrowthStage.Harvestable;
+                _waitingForWorker = true;
+            }
+        }
+
         /// <summary>
         /// Checks if the crop is ready to be harvested.
         /// </summary>
         private bool CheckGrowthAdvancement()
         {
-            if (_advancmentCompletionTime + growthTime >= Time.time)
+            if (_advancmentCompletionTime <= Time.time && !_waitingForWorker)
             {
                 return true;
             }
